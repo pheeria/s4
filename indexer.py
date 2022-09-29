c8fe0f937987d1fcb665d7dde84dbbf1296d33d4
pheeria <olzhas.askar@deliveryhero.com>
2022-09-29 18:29:50 +0200

    feat: inverted index

---
+++ indexer.py
@@ -0,0 +1,38 @@
+import re
+import json
+from unicodedata import normalize
+
+documents = []
+with open("./germany.json") as file:
+    documents = json.loads(file.read())
+
+index = {
+    "indices": {
+        "name": {},
+        "short_description": {},
+        "tags": {},
+    },
+    "source": {}
+}
+
+def tokenize(text: str) -> list: 
+    return [normalize("NFKD", token).encode("ASCII", "ignore").decode("ASCII")
+            for token in re.split(r"\W+", text.lower())]
+
+def populate(store: dict, tokens: list, source_index: int) -> None:
+    for token in tokens:
+        if token not in store:
+            store[token] = []
+        store[token].append(i)
+
+for i, document in enumerate(documents):
+    index["source"][i] = document
+    
+    restaurant = document["venue"]
+    populate(index["indices"]["name"], tokenize(restaurant["name"]), i)
+    populate(index["indices"]["short_description"], tokenize(restaurant["short_description"]), i)
+    populate(index["indices"]["tags"], restaurant["tags"], i)
+
+
+with open("./s4.json", "w") as file:
+    file.write(json.dumps(index, indent=4))
